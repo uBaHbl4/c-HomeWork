386ba80ee7b100766177bbf20e51403e51bc58b1
uBaHbl4 <iikornilov@icloud.com>
2023-10-22 12:45:32 +0500

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите номер дня недели (1-7): ");
+        int dayOfWeek = int.Parse(Console.ReadLine());
+
+        if (dayOfWeek == 6 || dayOfWeek == 7)
+        {
+            Console.WriteLine("Да, это выходной день.");
+        }
+        else if (dayOfWeek >= 1 && dayOfWeek <= 5)
+        {
+            Console.WriteLine("Нет, это не выходной день.");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный номер дня недели. Введите число от 1 до 7.");
+        }
+    }
+}
