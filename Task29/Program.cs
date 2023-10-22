928a9c31382be9e5019971de1befef14fc775ba6
uBaHbl4 <iikornilov@icloud.com>
2023-10-22 20:20:42 +0500

    TASK29

---
+++ Task29/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Answer
+{
+    static public void Main(string[] args)
+    {
+        int[] array = new int[8]; // Создаем массив из 8 элементов
+
+        if (args.Length >= 8)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                array[i] = int.Parse(args[i]); // Заполняем массив значениями из аргументов командной строки
+            }
+        }
+        else
+        {
+            // Измените значения по мере необходимости
+            array = new int[] { 1, 2, 5, 7, 19, 6, 1, 33 };
+        }
+
+        // Выводим массив на экран
+        Console.Write("[");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+            if (i < array.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+    }
+}
