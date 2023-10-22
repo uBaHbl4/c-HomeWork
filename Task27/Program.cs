d2db5efbc6c3b9d779da3b497ddaf4fe32d3f903
uBaHbl4 <iikornilov@icloud.com>
2023-10-22 20:18:06 +0500

    TASK27

---
+++ Task27/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Answer
+{
+    static int SumOfDigits(int number)
+    {
+        int sum = 0;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+
+        return sum;
+    }
+
+    static public void Main(string[] args)
+    {
+        int number;
+
+        if (args.Length >= 1)
+        {
+            number = int.Parse(args[0]);
+        }
+        else
+        {
+            // Измените значение number по мере необходимости
+            number = 111;
+        }
+
+        int result = SumOfDigits(number);
+        Console.WriteLine(result);
+    }
+}
