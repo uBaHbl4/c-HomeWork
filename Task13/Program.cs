cf7f1d05fc926b0cd6f4287491ad6cccbc0a2a4e
uBaHbl4 <iikornilov@icloud.com>
2023-10-22 12:41:37 +0500

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number >= 100 && number <= 999)
+        {
+            int thirdDigit = (number) % 10;
+            Console.WriteLine($"Третья цифра числа: {thirdDigit}");
+        }
+        else
+        {
+            Console.WriteLine("Третьей цифры нет.");
+        }
+    }
+}
