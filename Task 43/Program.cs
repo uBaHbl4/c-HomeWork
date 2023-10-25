1e72f518fd53e5418cf2d1ae41166b209dc3810f
uBaHbl4 <iikornilov@icloud.com>
2023-10-25 19:27:56 +0500

    homework6

---
+++ Task 43/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение b1: ");
+        double b1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение k1: ");
+        double k1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение b2: ");
+        double b2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение k2: ");
+        double k2 = double.Parse(Console.ReadLine());
+
+        // Рассчитываем точку пересечения двух прямых
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+
+        Console.WriteLine($"Точка пересечения: ({x:F1}; {y:F1})");
+    }
+}
---
+++ Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите числа, разделенные запятыми: ");
+        string input = Console.ReadLine();
+        
+        // Разбиваем строку на массив строк с помощью запятых
+        string[] inputNumbers = input.Split(',');
+
+        int countPositiveNumbers = 0;
+
+        foreach (string numberString in inputNumbers)
+        {
+            if (int.TryParse(numberString, out int number))
+            {
+                if (number > 0)
+                {
+                    countPositiveNumbers++;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Ошибка: '{numberString}' не является целым числом.");
+            }
+        }
+
+        Console.WriteLine($"Количество положительных чисел: {countPositiveNumbers}");
+    }
+}
